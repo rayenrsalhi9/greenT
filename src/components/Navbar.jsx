8cab3b0991a05f0934590a5159744957865cc5bb
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-03-01 13:04:01 +0000

    fixed the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
 import appIcon from '../assets/login-icon.png'
 import userIcon from '../assets/user.png'
+
+import menuIcon from '../assets/menu-burger.png'
+import closeMenuOpen from '../assets/cross.png'
+
 import '../styles/Navbar.css'
 
 export default function Navbar() {
+    const [isMenuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen)
+    }
+
     return (
         <nav>
             <Link to="/" className="logo">
@@ -21,6 +33,28 @@ export default function Navbar() {
             <Link to="/profile" className='user-icon'>
                 <img src={userIcon} alt="user icon" />
             </Link> 
+
+            <button className="mobile-menu-icon" onClick={toggleMenu}>
+                
+                {
+                    isMenuOpen ?
+                    <img src={closeMenuOpen} alt="close menu icon" /> :
+                    <img src={menuIcon} alt="mobile menu icon" />
+                }
+
+                {
+                    isMenuOpen &&
+                    <div className="mobile-menu">
+                        <Link to="">Profile</Link>
+                        <Link to="">Find Plastics</Link>
+                        <Link to="">Post Plastics</Link>
+                        <Link to="">How It Works</Link>
+                        <Link to="about">About GreenT</Link>
+                    </div>
+                }
+            </button>
+
+            
         </nav>
     )
 }
